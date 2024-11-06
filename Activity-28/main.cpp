8c8ccc06b1fcd53399ac84073cf155de28b8cb38
Juwon-14 <wndnjs9127@gmail.com>
2024-11-06 13:30:57 -0800

    Update main.cpp

--- Activity-28/main.cpp
+++ Activity-28/main.cpp
@@ -2,13 +2,17 @@
 //  main.cpp
 //  Activity-28
 //
-//  Created by 홍주원 on 11/6/24.
+//  Created by Juwon Hong on 11/6/24.
 //
 
 #include <iostream>
+#include "Converting.h"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    double number = 12.34;
+    cout << doubleToString(number) << endl;
     return 0;
 }
+
+
