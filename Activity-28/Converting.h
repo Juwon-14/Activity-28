285edd24ac996d67db5e50f779277533a9d256f4
Juwon-14 <wndnjs9127@gmail.com>
2024-11-06 13:30:30 -0800

    Update Converting.h

---
+++ Activity-28/Converting.h
@@ -0,0 +1,16 @@
+//
+//  Converting.h
+//  Activity-28
+//
+//  Created by Juwon Hong on 11/6/24.
+//
+
+#ifndef CONVERTING_H
+#define CONVERTING_H
+
+#include <string>
+using namespace std;
+
+string doubleToString(double number);
+
+#endif
